98b94e9c7ab8671b9050917fe0c19c4bfd558f80
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-07-07 15:16:16 -0400

    added create step stool

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,14 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params["id"])
     render template: "products/show"
   end
+
+  def create
+    @product = Product.create(
+      name: "step stool",
+      price: 30,
+      image_url: "https://i5.walmartimages.com/asr/9e7ce600-5d94-4042-9158-6caf19915761.763e34bb6172e5dffc9f9ed476293be7.jpeg",
+      description: "Step stool help you reach high places in your kitchen",
+    )
+    render :show
+  end
 end
