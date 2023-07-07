ab8c95178b275612087756dd757fa065f1a425a7
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-07-07 15:23:58 -0400

    updated create method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,10 +11,10 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.create(
-      name: "step stool",
-      price: 30,
-      image_url: "https://i5.walmartimages.com/asr/9e7ce600-5d94-4042-9158-6caf19915761.763e34bb6172e5dffc9f9ed476293be7.jpeg",
-      description: "Step stool help you reach high places in your kitchen",
+      name: params["name"],
+      price: params["price"],
+      image_url: params["image_url"],
+      description: params["description"],
     )
     render :show
   end
