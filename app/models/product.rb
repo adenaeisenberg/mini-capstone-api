9856ef02029e74c22fe6e750716a77381abd8cf3
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-07-20 14:37:38 -0400

    added category_products

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,17 +6,11 @@ class Product < ApplicationRecord
   validates :description, :presence => true
   validates :quantity, :presence => true
 
-  belongs_to :supplier
-  # def supplier
-  #   Supplier.find_by(id: supplier_id)
-  # end
-
-  has_many :images
-  # def images
-  #   Image.where(product_id: id)
-  # end
-
   has_many :orders
+  belongs_to :supplier
+  has_many :images
+  has_many :category_products
+  has_many :categories, through: :category_products
 
   def is_discounted?
     if price <= 10
